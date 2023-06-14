a872c0038da802c972ae95e15c8f92c923894e37
unknown <nag.multimedia@gmail.com>
2023-06-13 22:05:03 -0300

    se agregaron unos datos de prueba y una linea en el documento docker para levantar zipkin y poder ver el dashboard

--- CheckAgro/CheckAgro/api-customer/src/main/java/com/jmg/checkagro/customer/ApiCustomerApplication.java
+++ CheckAgro/CheckAgro/api-customer/src/main/java/com/jmg/checkagro/customer/ApiCustomerApplication.java
@@ -1,9 +1,15 @@
 package com.jmg.checkagro.customer;
 
+import com.jmg.checkagro.customer.model.Customer;
+import com.jmg.checkagro.customer.repository.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+import java.time.LocalDate;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -13,5 +19,19 @@ public class ApiCustomerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiCustomerApplication.class, args);
 	}
+/*
+	@Bean
+	public CommandLineRunner loadData(CustomerRepository repository) {
+		return (args) -> {
+			if (!repository.findAll().isEmpty()) {
+				return;
+			}
 
+			repository.save(new Customer(null, "DNI", "1111111", "", "", "", LocalDate.now(), true));
+			repository.save(new Customer(null, "DNI", "2222222", "", "", "", LocalDate.now(), true));
+			repository.save(new Customer(null, "DNI", "3333333", "", "", "", LocalDate.now(), true));
+			repository.save(new Customer(null, "DNI", "4444444", "", "", "", LocalDate.now(), true));
+		};
+	}
+*/
 }
--- CheckAgro/CheckAgro/api-provider/src/main/java/com/jmg/checkagro/provider/ApiProviderApplication.java
+++ CheckAgro/CheckAgro/api-provider/src/main/java/com/jmg/checkagro/provider/ApiProviderApplication.java
@@ -1,9 +1,15 @@
 package com.jmg.checkagro.provider;
 
+import com.jmg.checkagro.provider.model.Provider;
+import com.jmg.checkagro.provider.repository.ProviderRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import java.time.LocalDate;
 
 @SpringBootApplication
 @EnableJpaRepositories
@@ -12,6 +18,20 @@ public class ApiProviderApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiProviderApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner loadData(ProviderRepository repository) {
+		return (args) -> {
+			if (!repository.findAll().isEmpty()) {
+				return;
+			}
+
+			repository.save(new Provider(null, "DNI", "6666666", "", "", "", LocalDate.now(), true));
+			repository.save(new Provider(null, "DNI", "7777777", "", "", "", LocalDate.now(), true));
+			repository.save(new Provider(null, "DNI", "8888888", "", "", "", LocalDate.now(), true));
+			repository.save(new Provider(null, "DNI", "9999999", "", "", "", LocalDate.now(), true));
+		};
 	}
 
 }
